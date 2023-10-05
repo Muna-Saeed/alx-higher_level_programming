ada5428de9d0942631cd2ab6730f7f18cd4ee247
Muna-Saeed <munasaeed6367@gmail.com>
2023-10-04 18:02:21 -0700

    updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{0}{1}".format(chr(i), chr(i - 32)), end='')
-
-print()
+    case = chr(i - 32) if i % 2 == 0 else chr(i)
+    print(case, end="")
