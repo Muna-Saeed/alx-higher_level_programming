b85e022320b5bbf283a64119c04d9f23ff2da0ff
Muna-Saeed <munasaeed6367@gmail.com>
2023-10-30 14:56:43 -0700

    4. Eval is magic

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""A class representing a rectangle."""
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        """Initialize the rectangle with the given width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            rect_str = ""
+            for _ in range(self.__height):
+                rect_str +="#" * self.__width + "\n"
+            return rect_str.rstrip()
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self.__width == other.width and self.__height == other.height
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, Rectangle):
+            return self.area() < other.area()
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, Rectangle):
+            return self.area() > other.area()
+        return NotImplemented
