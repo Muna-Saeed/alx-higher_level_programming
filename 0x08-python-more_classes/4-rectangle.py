aab405181de39e4daa57dc0c6cd16e10ddb6a1b6
Muna-Saeed <munasaeed6367@gmail.com>
2023-11-01 16:14:56 -0700

    4. Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -3,6 +3,8 @@
 
 
 class Rectangle:
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
         """Initialize the rectangle with the given width and height."""
         self.width = width
@@ -38,6 +40,8 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
