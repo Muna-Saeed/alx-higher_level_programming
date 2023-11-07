533e757700066717739ef0568ba0e36b886e24b2
Muna-Saeed <munasaeed6367@gmail.com>
2023-11-07 13:31:22 -0800

    updated

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" dictionary description with a simple data structure for JSON serialization of an object."""
+"""dictionary description with simple data structure for JSON an object."""
 
 
 def class_to_json(obj):
-    """"A dictionary representing the object's attributes in a JSON serializable format."""
+    """"A dictionary the object's attributes in a JSON serializable format."""
     if hasattr(obj, "__dict__"):
         return obj.__dict__
     elif hasattr(obj, "__slots__"):
         return {slot: getattr(obj, slot) for slot in obj.__slots__}
     else:
-        raise TypeError(f"Object of type {type(obj).__name__} is not serializable.")
+        raise TypeError("Object of type {} is not serializable."
+                        .format(type(obj).__name__))
