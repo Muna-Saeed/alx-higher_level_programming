35a3cbe096df46decb2f93c95d6f97adada978e4
Muna-Saeed <munasaeed6367@gmail.com>
2023-11-07 13:33:27 -0800

    updated

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,14 +9,11 @@ if __name__ == "__main__":
         __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
-# Load the existing list from the file, if it exists
 try:
     my_list = load_from_json_file(filename)
 except FileNotFoundError:
     my_list = []
 
-# Add the arguments to the list
 my_list.extend(sys.argv[1:])
 
-# Save the updated list to the file
 save_to_json_file(my_list, filename)
