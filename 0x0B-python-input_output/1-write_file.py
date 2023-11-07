e63bdd0b4dc53b90057cd8640ceb13b724d85d83
Muna-Saeed <munasaeed6367@gmail.com>
2023-11-07 11:40:55 -0800

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename="", text=""):
+    """a function that writes a string to a text file"""
     with open(filename, mode="w", encoding="utf-8") as file:
         return file.write(text)
