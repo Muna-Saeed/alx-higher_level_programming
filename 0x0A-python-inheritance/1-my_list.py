7fc3714844d1e152fd833f9ff3ad7ebc816fe487
Muna-Saeed <munasaeed6367@gmail.com>
2023-11-09 01:35:44 -0800

    mandatory tasks

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""print_sorted"""
+
+
+class MyList(list):
+    """a class MyList that inherits from list"""
+    def print_sorted(self):
+        """public instnce method that prints the list, but sorted"""
+        print(sorted(self))
+
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""is_kind_of_class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if obj is an instance of or inherited from a_class, else False."""
+    return isinstance(obj, a_class)
