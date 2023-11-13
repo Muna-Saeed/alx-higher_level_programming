77031e895172d3853fc92e5061aabc65a8003c24
Muna-Saeed <munasaeed6367@gmail.com>
2023-11-13 13:57:47 -0800

    17. JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,107 @@
 #!/usr/bin/python3
 """Base class module"""
+import json
 
 
 class Base:
     """
-    Base class that manages the id attribute for all other classes in the project.
+    Base class for other classes in the project.
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initializes a Base instance."""
+        """
+        Initializes a Base instance.
+
+        Args:
+            id (int): The id of the Base instance. Defaults to None.
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+
+        Returns:
+            str: The JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list represented by the JSON string json_string.
+
+        Args:
+            json_string (str): A string representing a list of dictionaries.
+
+        Returns:
+            list: The list represented by json_string.
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs (list): A list of instances that inherit from Base.
+        """
+        filename = cls.__name__ + ".json"
+        json_list = []
+        if list_objs is not None:
+            json_list = [obj.to_dictionary() for obj in list_objs]
+        with open(filename, "w") as file:
+            file.write(cls.to_json_string(json_list))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Creates an instance of the class using the attributes in dictionary.
+
+        Args:
+            dictionary (dict): A dictionary of attributes.
+
+        Returns:
+            Base: An instance of the class with the attributes from dictionary.
+        """
+        if cls.__name__ == "Rectangle":
+            new_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            new_instance = cls(1)
+        else:
+            new_instance = None
+        new_instance.update(**dictionary)
+        return new_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Loads instances from a JSON file.
+
+        Returns:
+            list: A list of instances.
+        """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                json_data = file.read()
+                data = cls.from_json_string(json_data)
+                instances = [cls.create(**d) for d in data]
+                return instances
+        except FileNotFoundError:
+            return []
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -23,6 +23,63 @@ class TestBase(unittest.TestCase):
         b6 = Base()
         self.assertIsInstance(b6.id, int)
 
+    
+    def test_to_json_string_empty(self):
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
+
+    def test_to_json_string(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_string = Base.to_json_string([dictionary])
+        self.assertEqual(json_string, '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]')
+
+    def test_from_json_string_empty(self):
+        list_objs = Base.from_json_string(None)
+        self.assertEqual(list_objs, [])
+
+    def test_from_json_string(self):
+        json_string = '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]'
+        list_objs = Base.from_json_string(json_string)
+        self.assertEqual(list_objs, [{'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}])
+
+    def test_save_to_file_rectangle(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+
+        with open("Rectangle.json", "r") as file:
+            json_data = file.read()
+            self.assertEqual(json_data, '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}, '
+                                         '{"x": 0, "width": 2, "id": 2, "height": 4, "y": 0}]')
+
+    def test_save_to_file_square(self):
+        s1 = Square(5)
+        s2 = Square(7, 9, 1)
+        Square.save_to_file([s1, s2])
+
+        with open("Square.json", "r") as file:
+            json_data = file.read()
+            self.assertEqual(json_data, '[{"id": 1, "size": 5, "x": 0, "y": 0}, '
+                                         '{"id": 2, "size": 7, "x": 9, "y": 1}]')
+
+    def test_load_from_file_rectangle(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        rectangles = Rectangle.load_from_file()
+        self.assertEqual(len(rectangles), 2)
+        self.assertEqual(rectangles[0].to_dictionary(), r1.to_dictionary())
+        self.assertEqual(rectangles[1].to_dictionary(), r2.to_dictionary())
+
+    def test_load_from_file_square(self):
+        s1 = Square(5)
+        s2 = Square(7, 9, 1)
+        Square.save_to_file([s1, s2])
+        squares = Square.load_from_file()
+        self.assertEqual(len(squares), 2)
+        self.assertEqual(squares[0].to_dictionary(), s1.to_dictionary())
+        self.assertEqual(squares[1].to_dictionary(), s2.to_dictionary())
 
 if __name__ == '__main__':
     unittest.main()
