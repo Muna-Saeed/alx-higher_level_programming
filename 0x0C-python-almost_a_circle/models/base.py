06f4f11332615cb1ed538e8a8e9955044795a963
Muna-Saeed <munasaeed6367@gmail.com>
2023-11-13 14:28:39 -0800

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """Base class module"""
 import json
+import csv
 
 
 class Base:
     """
-    Base class for other classes in the project.
+    Base class for serialization, deserialization, and drawing of rectangles and squares.
     """
-
     __nb_objects = 0
 
     def __init__(self, id=None):
         """
-        Initializes a Base instance.
+        Initializes an instance of the Base class.
 
         Args:
-            id (int): The id of the Base instance. Defaults to None.
+            id (int): The ID of the instance. If not provided, it will be automatically assigned.
         """
         if id is not None:
             self.id = id
@@ -24,15 +24,125 @@ class Base:
             self.id = Base.__nb_objects
 
     @staticmethod
+    def to_csv_row(instance):
+        """
+        Converts an instance of a Rectangle or Square to a CSV row.
+
+        Args:
+            instance (Rectangle or Square): The instance to convert.
+
+        Returns:
+            list: The CSV row representing the instance.
+        """
+        if isinstance(instance, Rectangle):
+            return [instance.id, instance.width, instance.height, instance.x, instance.y]
+        elif isinstance(instance, Square):
+            return [instance.id, instance.size, instance.x, instance.y]
+
+    @staticmethod
+    def from_csv_row(row):
+        """
+        Creates an instance of Rectangle or Square from a CSV row.
+
+        Args:
+            row (list): The CSV row representing the instance.
+
+        Returns:
+            Rectangle or Square: The created instance.
+        """
+        if len(row) == 5:
+            return Rectangle(*row[1:])
+        elif len(row) == 4:
+            return Square(*row[1:])
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        Serializes a list of Rectangle or Square instances to a CSV file.
+
+        Args:
+            list_objs (list): The list of instances to serialize.
+        """
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            if list_objs is not None:
+                for obj in list_objs:
+                    writer.writerow(cls.to_csv_row(obj))
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        Deserializes a list of Rectangle or Square instances from a CSV file.
+
+        Returns:
+            list: The deserialized list of instances.
+        """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, mode='r', newline='') as file:
+                reader = csv.reader(file)
+                instances = []
+                for row in reader:
+                    instance = cls.from_csv_row(row)
+                    instances.append(instance)
+                return instances
+        except FileNotFoundError:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """
+        Opens a window and draws the rectangles and squares using Turtle graphics.
+
+        Args:
+            list_rectangles (list): The list of Rectangle instances to draw.
+            list_squares (list): The list of Square instances to draw.
+        """
+        import turtle
+        
+        def draw_rectangle(rectangle):
+            turtle.penup()
+            turtle.goto(rectangle.x, rectangle.y)
+            turtle.pendown()
+            for _ in range(2):
+                turtle.forward(rectangle.width)
+                turtle.right(90)
+                turtle.forward(rectangle.height)
+                turtle.right(90)
+            turtle.penup()
+
+        def draw_square(square):
+            turtle.penup()
+            turtle.goto(square.x, square.y)
+            turtle.pendown()
+            for _ in range(4):
+                turtle.forward(square.size)
+                turtle.right(90)
+            turtle.penup()
+
+        turtle.speed(0)
+        turtle.shape("turtle")
+
+        for rectangle in list_rectangles:
+            draw_rectangle(rectangle)
+
+        for square in list_squares:
+            draw_square(square)
+
+        turtle.exitonclick()
+
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
-        Returns the JSON string representation of list_dictionaries.
+        Converts a list of dictionaries to a JSON string representation.
 
         Args:
             list_dictionaries (list): A list of dictionaries.
 
         Returns:
-            str: The JSON string representation of list_dictionaries.
+            str: The JSON string representation of the list of dictionaries.
+
         """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
@@ -41,13 +151,14 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """
-        Returns the list represented by the JSON string json_string.
+        Converts a JSON string representation to a list of dictionaries.
 
         Args:
-            json_string (str): A string representing a list of dictionaries.
+            json_string (str): The JSON string representation.
 
         Returns:
-            list: The list represented by json_string.
+            list: A list of dictionaries.
+
         """
         if json_string is None or len(json_string) == 0:
             return []
@@ -56,10 +167,11 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """
-        Writes the JSON string representation of list_objs to a file.
+        Saves a list of instances to a file in JSON format.
 
         Args:
-            list_objs (list): A list of instances that inherit from Base.
+            list_objs (list): A list of instances.
+
         """
         filename = cls.__name__ + ".json"
         json_list = []
@@ -69,39 +181,40 @@ class Base:
             file.write(cls.to_json_string(json_list))
 
     @classmethod
-    def create(cls, **dictionary):
-        """
-        Creates an instance of the class using the attributes in dictionary.
-
-        Args:
-            dictionary (dict): A dictionary of attributes.
-
-        Returns:
-            Base: An instance of the class with the attributes from dictionary.
-        """
-        if cls.__name__ == "Rectangle":
-            new_instance = cls(1, 1)
-        elif cls.__name__ == "Square":
-            new_instance = cls(1)
-        else:
-            new_instance = None
-        new_instance.update(**dictionary)
-        return new_instance
-
-    @classmethod
     def load_from_file(cls):
         """
-        Loads instances from a JSON file.
+        Loads a list of instances from a file in JSON format.
 
         Returns:
             list: A list of instances.
+
         """
         filename = cls.__name__ + ".json"
         try:
             with open(filename, "r") as file:
                 json_data = file.read()
-                data = cls.from_json_string(json_data)
-                instances = [cls.create(**d) for d in data]
-                return instances
+                dictionaries = cls.from_json_string(json_data)
+                return [cls.create(**dictionary) for dictionary in dictionaries]
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Creates an instance with all attributes already set based on a dictionary.
+
+        Args:
+            **dictionary: Double pointer to a dictionary containing the attributes.
+
+        Returns:
+            object: An instance of the class with attributes set according to the dictionary.
+
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy = None
+        dummy.update(**dictionary)
+        return dummy
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+"""Unittests for Base class"""
 import unittest
+import json
+import os
+from models.rectangle import Rectangle
+from models.square import Square
 from models.base import Base
 
 
@@ -63,23 +69,123 @@ class TestBase(unittest.TestCase):
             self.assertEqual(json_data, '[{"id": 1, "size": 5, "x": 0, "y": 0}, '
                                          '{"id": 2, "size": 7, "x": 9, "y": 1}]')
 
+    
+
+    def test_create_rectangle(self):
+        """Test create method for Rectangle"""
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertIsInstance(r2, Rectangle)
+        self.assertEqual(r1, r2)
+        self.assertIsNot(r1, r2)
+
+    def test_create_square(self):
+        """Test create method for Square"""
+        s1 = Square(5)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+        self.assertIsInstance(s2, Square)
+        self.assertEqual(s1, s2)
+        self.assertIsNot(s1, s2)
+
     def test_load_from_file_rectangle(self):
+        """Test load_from_file method for Rectangle"""
         r1 = Rectangle(10, 7, 2, 8)
         r2 = Rectangle(2, 4)
-        Rectangle.save_to_file([r1, r2])
-        rectangles = Rectangle.load_from_file()
-        self.assertEqual(len(rectangles), 2)
-        self.assertEqual(rectangles[0].to_dictionary(), r1.to_dictionary())
-        self.assertEqual(rectangles[1].to_dictionary(), r2.to_dictionary())
+        list_rectangles_input = [r1, r2]
+
+        """Save rectangles to file"""
+        Rectangle.save_to_file(list_rectangles_input)
+
+        """Load rectangles from file"""
+        list_rectangles_output = Rectangle.load_from_file()
+
+        self.assertIsInstance(list_rectangles_output, list)
+        self.assertEqual(len(list_rectangles_output), 2)
+
+        """Check if loaded rectangles match original rectangles"""
+        for rect_input, rect_output in zip(list_rectangles_input, list_rectangles_output):
+            self.assertIsInstance(rect_output, Rectangle)
+            self.assertEqual(rect_input, rect_output)
+            self.assertIsNot(rect_input, rect_output)
+
+        """Delete the created file"""
+        os.remove("Rectangle.json")
 
     def test_load_from_file_square(self):
+        """Test load_from_file method for Square"""
         s1 = Square(5)
         s2 = Square(7, 9, 1)
-        Square.save_to_file([s1, s2])
-        squares = Square.load_from_file()
-        self.assertEqual(len(squares), 2)
-        self.assertEqual(squares[0].to_dictionary(), s1.to_dictionary())
-        self.assertEqual(squares[1].to_dictionary(), s2.to_dictionary())
+        list_squares_input = [s1, s2]
+
+        """Save squares to file"""
+        Square.save_to_file(list_squares_input)
+
+        """Load squares from file"""
+        list_squares_output = Square.load_from_file()
+
+        self.assertIsInstance(list_squares_output, list)
+        self.assertEqual(len(list_squares_output), 2)
+
+        """Check if loaded squares match original squares"""
+        for square_input, square_output in zip(list_squares_input, list_squares_output):
+            self.assertIsInstance(square_output, Square)
+            self.assertEqual(square_input, square_output)
+            self.assertIsNot(square_input, square_output)
+
+        """Delete the created file"""
+        os.remove("Square.json")
+
+
+def test_csv_serialization_deserialization():
+    """
+    Test CSV serialization and deserialization.
+    """
+    r1 = Rectangle(10, 7, 2, 8)
+    r2 = Rectangle(2, 4)
+    list_rectangles_input = [r1, r2]
+
+    Rectangle.save_to_file_csv(list_rectangles_input)
+
+    list_rectangles_output = Rectangle.load_from_file_csv()
+
+    for rect in list_rectangles_input:
+        print("[{}] {}".format(id(rect), rect))
+
+    print("---")
+
+    for rect in list_rectangles_output:
+        print("[{}] {}".format(id(rect), rect))
+
+    print("---")
+    print("---")
+
+    s1 = Square(5)
+    s2 = Square(7, 9, 1)
+    list_squares_input = [s1, s2]
+
+    Square.save_to_file_csv(list_squares_input)
+
+    list_squares_output = Square.load_from_file_csv()
+
+    for square in list_squares_input:
+        print("[{}] {}".format(id(square), square))
+
+    print("---")
+
+    for square in list_squares_output:
+        print("[{}] {}".format(id(square), square))
+
+def test_drawing():
+    """
+    Test drawing rectangles and squares using Turtle graphics.
+    """
+    list_rectangles = [Rectangle(100, 40), Rectangle(90, 110, 30, 10), Rectangle(20, 25, 110, 80)]
+    list_squares = [Square(35), Square(15, 70, 50), Square(80, 30, 70)]
+
+    Base.draw(list_rectangles, list_squares)
+
 
 if __name__ == '__main__':
     unittest.main()
