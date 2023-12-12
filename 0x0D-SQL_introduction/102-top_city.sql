c4bb8d395ee51515c86e29a9a8bed34f5be502b5
Muna-Saeed <munasaeed6367@gmail.com>
2023-12-12 14:46:52 -0800

    19. Temperatures #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,9 +1,10 @@
 -- Task: Top 3 Cities by Temperature
 -- This script displays the top 3 cities with the highest temperatures during July and August, ordered by temperature (descending).
 -- cat 102-top_city.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT city, AVG(temperature) AS avg_temp
+
+SELECT city, AVG(`value`) AS avg_temp
 FROM temperatures
-WHERE MONTH(date) IN (7, 8)
+WHERE `month` = 7 OR `month` = 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
