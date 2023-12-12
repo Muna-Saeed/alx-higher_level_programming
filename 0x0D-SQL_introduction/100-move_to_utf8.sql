fd213188fb6be3f99cef3febeecb6b1898f3a810
Muna-Saeed <munasaeed6367@gmail.com>
2023-12-12 14:34:39 -0800

    17. Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
 -- Convert hbtn_0c_0 database to UTF8
 -- cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p 
+USE hbtn_0c_0;
 
 -- Convert the first_table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
