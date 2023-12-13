a7f0f0c44ff9883b26ff22ac9ef84fc5044e307e
Muna-Saeed <munasaeed6367@gmail.com>
2023-12-13 15:38:30 -0800

    16. List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- Use the hbtn_0d_tvshows database
-USE hbtn_0d_tvshows;
-
 -- List all shows and their genres
+
 SELECT title, name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
