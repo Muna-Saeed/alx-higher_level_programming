1c685adf467669055f0e5b929a0fbb65026d1be1
Muna-Saeed <munasaeed6367@gmail.com>
2023-12-13 15:45:47 -0800

    done

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,11 +1,9 @@
 -- Use the hbtn_0d_tvshows_rate database
-USE hbtn_0d_tvshows_rate;
 
 -- List all genres by their rating
-SELECT tv_genres.name, SUM(tv_shows_rate.rating) as rating_sum
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-LEFT JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.tv_show_id
-GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
