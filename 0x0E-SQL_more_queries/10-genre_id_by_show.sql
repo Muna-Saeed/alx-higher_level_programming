963bdb3f3e18607db1454135e19715c58b24dbcc
Muna-Saeed <munasaeed6367@gmail.com>
2023-12-13 15:00:52 -0800

    10. Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,9 @@
 -- Task: List Genre ID by Show
 -- This script lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 
+-- Use the database hbtn_0d_tvshows
+USE hbtn_0d_tvshows;
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
