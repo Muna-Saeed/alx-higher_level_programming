e1f06eb428fc2fed423a83c248e39b1a92607105
Muna-Saeed <munasaeed6367@gmail.com>
2023-12-13 15:17:32 -0800

    done

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,7 @@
 -- Use the database hbtn_0d_tvshows
-USE hbtn_0d_tvshows;
-
 -- Select tv_shows.title and tv_show_genres.genre_id from relevant tables
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
