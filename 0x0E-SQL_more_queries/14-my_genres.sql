09df7d7fcbabcb2c01e53899cbafc249a59f8164
Muna-Saeed <munasaeed6367@gmail.com>
2023-12-13 15:31:08 -0800

    14. My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- Task: My Genres
+-- This script lists all genres of the show Dexter from the hbtn_0d_tvshows database.
+
+SELECT name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+GROUP BY name
+ORDER BY name ASC;
