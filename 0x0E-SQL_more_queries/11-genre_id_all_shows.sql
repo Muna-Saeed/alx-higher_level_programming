cae1b457b6be21effa2308ace8e76118f81fe7cf
Muna-Saeed <munasaeed6367@gmail.com>
2023-12-13 15:21:30 -0800

    done

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,6 +2,6 @@
 -- This script lists all shows contained in the hbtn_0d_tvshows database and displays their corresponding genre IDs.
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
