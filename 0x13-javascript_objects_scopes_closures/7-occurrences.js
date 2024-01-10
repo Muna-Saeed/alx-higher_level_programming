c6ee5d537ef6d0dce60d58449e9def52602423f8
Muna Saeed <munasaeed6367@gmail.com>
2024-01-11 00:17:06 +0300

    done

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  // Use the filter method to count occurrences
+  const occurrences = list.filter(element => element === searchElement);
+  return occurrences.length;
+};
