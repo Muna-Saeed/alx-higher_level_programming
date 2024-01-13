c62964c9de4700ea373b42ee419dea9adc8b99d5
Muna Saeed <munasaeed6367@gmail.com>
2024-01-13 14:09:04 -0800

    done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-"""
-Script that lists all cities from the database hbtn_0e_4_usa.
-"""
-
-import MySQLdb
+""" list all states order by id"""
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-            host="localhost", port=3306,
-            user=username, passwd=password, db=database
-            )
-    cursor = db.cursor()
+if __name__ == "__main__" and len(sys.argv) == 5:
+    user = sys.argv[1]
+    passw = sys.argv[2]
+    db = sys.argv[3]
+    state = sys.argv[4]
+    con = MySQLdb.connect(host='localhost',
+                          port=3306, user=user,
+                          passwd=passw, db=db)
+    cur = con.cursor()
+    qr = "SELECT cities.name FROM cities JOIN"
+    qr += " states ON cities.state_id = states.id"
+    qr += " WHERE states.name = %s ORDER BY cities.id;"
+    cur.execute(qr, (state, ))
 
-    # Execute JOIN query to get cities with corresponding states
-    query = """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC
-    """
-    cursor.execute(query)
-
-    # Fetch all rows
-    rows = cursor.fetchall()
-
-    # Display results
-    for row in rows:
-        print(row)
-
-    # Close cursor and database connection
-    cursor.close()
-    db.close()
+    print(", ".join(map(lambda x: x[0], cur.fetchall())))
+    cur.close()
+    con.close()
