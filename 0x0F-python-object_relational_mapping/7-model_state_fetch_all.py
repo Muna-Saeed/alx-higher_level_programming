b230a7956c6379c7d5b6be6a22f4a00574f736c9
Muna Saeed <munasaeed6367@gmail.com>
2024-01-13 15:00:40 -0800

    done

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,12 @@ import sys
 
 if __name__ == "__main__":
     # Create a connection to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]
+                ),
+            pool_pre_ping=True
+            )
 
     # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
