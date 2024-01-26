a5361e770c6b55160087b0a3022080dc84ced199
Muna Saeed <munasaeed6367@gmail.com>
2024-01-26 12:32:38 -0800

    done

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script for finding peak in list of ints
+"""
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers.
+    """
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            # If the mid element is greater than its right neighbor, move left
+            high = mid
+        else:
+            # If the mid element is less than or equal to its right neighbor, move right
+            low = mid + 1
+
+    # At the end of the loop, low and high will be pointing to the peak element
+    return list_of_integers[low]
