c950cd9c7334347606e64cce1d29e673d41addf8
Muna Saeed <munasaeed6367@gmail.com>
2024-01-26 10:46:26 -0800

    done

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Send a GET request using curl and display the body of the response for a 200 status code
-curl -sL "$1" -w "%{http_code}" -o /dev/null | {
-    read -r status
-    [ "$status" -eq 200 ] && curl -s "$1"
-}
+curl -sX GET $1 -L
