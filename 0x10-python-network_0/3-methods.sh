4d05b662bdb640cabd063c6031affa5a974546b4
Muna Saeed <munasaeed6367@gmail.com>
2024-01-26 12:00:13 -0800

    done

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send an OPTIONS request using curl and display the allowed methods
-curl -sI "$1" -X OPTIONS | grep -i "Allow" | awk '{print $2}'
+curl -sI "$1" -X OPTIONS | grep -i "Allow" | cut -d " " -f2-
