f6b7577482ce2f072d71c51f2191cce2eafc560c
Muna Saeed <munasaeed6367@gmail.com>
2024-01-27 05:18:50 -0800

    done..

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request to the specified URL that causes the server to respond with a message containing You got me!
-curl -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
