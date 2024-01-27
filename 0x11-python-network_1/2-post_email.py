6f4aa734fa9ad9958e4cd99b3d388c71abceb930
Muna Saeed <munasaeed6367@gmail.com>
2024-01-27 09:18:55 -0800

    done

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that sends a POST request to a specified URL
+with an email as a parameter and then displays
+the body of the response.
+"""
+
+
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    # Assign URL and email from command line arguments
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Encode the email parameter
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    # Make the POST request
+    with urllib.request.urlopen(url, data) as response:
+        # Read and decode the response body
+        body = response.read().decode('utf-8')
+        print("Your email is: {}".format(body))
