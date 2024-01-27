0e7419ab25a301cd4b8e1454e5741b7aeb204495
Muna Saeed <munasaeed6367@gmail.com>
2024-01-27 05:04:15 -0800

    done..

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request to the specified URL and follow the redirection
-curl -s -L -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sw "You got me!" -L -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
