0ea56b85a86371e6cf8a67c7ba1e36fcca0c897d
Muna Saeed <munasaeed6367@gmail.com>
2024-02-29 05:00:57 -0800

    done

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,4 +1,4 @@
 // Updates the text color of the <header> element to red (#FF0000) when the document is loaded
-
-document.querySelector('header').style.color = '#FF0000';
-
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('header').style.color = '#FF0000';
+});
