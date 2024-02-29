61a2ab15e513ec9465949737853375bc4372d3c4
Muna Saeed <munasaeed6367@gmail.com>
2024-02-29 04:01:05 -0800

    done

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,4 @@
+// Updates the text of the header element to "New Header!!!" when the div#update_header tag is clicked
+$('div#update_header').click(function () {
+    $('header').text('New Header!!!');
+});
